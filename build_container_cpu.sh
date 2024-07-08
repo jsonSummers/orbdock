a62f5691c1a644977a737d321a6711d15c3bfadf
Jason Summers <135453734+jsonSummers@users.noreply.github.com>
2024-07-08 11:41:10 +0100

    Add files via upload

--- build_container_cpu.sh
+++ build_container_cpu.sh
@@ -8,10 +8,22 @@ replace_files() {
     cp Modified_files/MapPoint.h ORB_SLAM3/include/MapPoint.h
 }
 
+
+###########################################################################
+
+# UI permisions
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+touch $XAUTH
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
+xhost +local:docker
+
 docker pull jahaniam/orbslam3:ubuntu20_noetic_cpu
 
-# Remove existing container if exists
+# Remove existing container
 docker rm -f orbslam3 &>/dev/null
+[ -d "ORB_SLAM3" ] && sudo rm -rf ORB_SLAM3 && mkdir ORB_SLAM3
 
 # Create a new container
 docker run -td --privileged --net=host --ipc=host \
@@ -22,11 +34,12 @@ docker run -td --privileged --net=host --ipc=host \
     -e "XAUTHORITY=$XAUTH" \
     -e ROS_IP=127.0.0.1 \
     --cap-add=SYS_PTRACE \
-    -v "$(pwd)/Datasets:/Datasets" \
+    -v `pwd`/Datasets:/Datasets \
     -v /etc/group:/etc/group:ro \
-    -v "$(pwd)/ORB_SLAM3:/ORB_SLAM3" \
-    -v "$(pwd)/study:/study" \  # Added volume mount for study folder
+    -v `pwd`/ORB_SLAM3:/ORB_SLAM3 \
+    -v "$(pwd)/study:/study" \
     jahaniam/orbslam3:ubuntu20_noetic_cpu bash
+
 
 # Git pull orbslam and compile (using HTTPS)
 #docker exec -i orbslam3 bash -c "git clone -b add_euroc_example.sh https://github.com/jahaniam/ORB_SLAM3.git /ORB_SLAM3 && cd /ORB_SLAM3 && chmod +x build.sh && ./build.sh "
